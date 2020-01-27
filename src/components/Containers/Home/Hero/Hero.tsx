79ee5d95388e90fb37fe1893a74e68e361788a66
Nir Parisian <nir.parisian@gmail.com>
2020-01-27 22:36:39 +0200

    code structure fixes + Podcast Preview skeleton

--- src/components/Containers/Home/Hero/Hero.tsx
+++ src/components/Containers/Home/Hero/Hero.tsx
@@ -1,3 +1,45 @@
 import React from "react"
+import styled from "styled-components"
 
-export default () => <div>Hero</div>
+import Image from "../../../image"
+
+export default () => (
+  <StyledHero>
+    <StyledWelcome>
+      <h2>העתיד שאתה בא ממנו</h2>
+      <p>
+        לורם איפסום דולור סיט אמט, קונסקטורר אדיפיסינג אלית קולהע צופעט למרקוח
+        איבן איף, ברומץ כלרשט מיחוצים. קלאצי נולום ארווס סאפיאן - פוסיליס קוויס,
+        אקווזמן קוואזי במר מודוף. אודיפו בלאסטיק מונופץ קליר, בנפת נפקט למסון
+        בלרק - וענוף לורם איפסום דולור סיט אמט, לורם איפסום דולור סיט אמט,
+        קונסקטורר אדיפיסינג אלית. סת אלמנקום ניסי נון ניבאה. דס איאקוליס וולופטה
+        דיאם. וסטיבולום אט דולור, קראס אגת לקטוס וואל אאוגו וסטיבולום סוליסי
+        טידום בעליק. להאמית קרהשק סכעיט דז מא, מנכם למטכין נשואי מנורך. צש בליא,
+        מנסוטו צמלח לביקו ננבי, צמוקו בלוקריה שיצמה ברורק. הועניב היושבב שערש
+        שמחויט - שלושע ותלברו חשלו שעותלשך וחאית נובש ערששף. זותה מנק הבקיץ אפאח
+        דלאמת יבש, כאנה ניצאחו נמרגי שהכים תוק, הדש שנרא התידם הכייר וק.
+      </p>
+    </StyledWelcome>
+    <StyledLogo>
+      <Image />
+    </StyledLogo>
+  </StyledHero>
+)
+
+const StyledHero = styled.div`
+  display: flex;
+  flex-direction: row-reverse;
+  width: 100%;
+`
+
+const StyledWelcome = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+  text-align: right;
+  flex: 0.66;
+`
+const StyledLogo = styled.div`
+  display: flex;
+  flex: 0.34;
+`
--- src/components/Containers/Home/Home.tsx
+++ src/components/Containers/Home/Home.tsx
@@ -1,52 +1,18 @@
 import React from "react"
 import styled from "styled-components"
-import Image from "../../image"
+
+import PodcastPreview from "./PodcastPreview/PodcastPreview"
+import Hero from "./Hero/Hero"
 
 export default () => (
   <StyledHome>
-    <StyledHero>
-      <StyledWelcome>
-        <h2>העתיד שאתה בא ממנו</h2>
-        <p>
-          לורם איפסום דולור סיט אמט, קונסקטורר אדיפיסינג אלית קולהע צופעט למרקוח
-          איבן איף, ברומץ כלרשט מיחוצים. קלאצי נולום ארווס סאפיאן - פוסיליס
-          קוויס, אקווזמן קוואזי במר מודוף. אודיפו בלאסטיק מונופץ קליר, בנפת נפקט
-          למסון בלרק - וענוף לורם איפסום דולור סיט אמט, לורם איפסום דולור סיט
-          אמט, קונסקטורר אדיפיסינג אלית. סת אלמנקום ניסי נון ניבאה. דס איאקוליס
-          וולופטה דיאם. וסטיבולום אט דולור, קראס אגת לקטוס וואל אאוגו וסטיבולום
-          סוליסי טידום בעליק. להאמית קרהשק סכעיט דז מא, מנכם למטכין נשואי מנורך.
-          צש בליא, מנסוטו צמלח לביקו ננבי, צמוקו בלוקריה שיצמה ברורק. הועניב
-          היושבב שערש שמחויט - שלושע ותלברו חשלו שעותלשך וחאית נובש ערששף. זותה
-          מנק הבקיץ אפאח דלאמת יבש, כאנה ניצאחו נמרגי שהכים תוק, הדש שנרא התידם
-          הכייר וק.
-        </p>
-      </StyledWelcome>
-      <StyledLogo>
-        <Image />
-      </StyledLogo>
-    </StyledHero>
+    <Hero />
+    <PodcastPreview />
   </StyledHome>
 )
 
 const StyledHome = styled.section`
   display: flex;
-  flex-direction: row-reverse;
-`
-
-const StyledHero = styled.div`
-  display: flex;
-  flex-direction: row-reverse;
-  width: 100%;
-`
-
-const StyledWelcome = styled.div`
-  display: flex;
   flex-direction: column;
-  align-items: flex-end;
-  text-align: right;
-  flex: 0.66;
-`
-const StyledLogo = styled.div`
-  display: flex;
-  flex: 0.34;
+  align-items: flex-end; // elements starts from right to left
 `
